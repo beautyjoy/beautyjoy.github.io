d0741cf6a2458f959b24c84708b86a5b897df1ad
Omoju <omojumiller@gmail.com>
2013-11-05 11:14:22 -0800

    BJC website, first commit

---
+++ js/submit.js
@@ -0,0 +1,40 @@
+/*
+Creating an HTML5 enhanced responsive-ready contact form, with custom javascript feature detection
+www.toddmotto.com
+*/
+(function() {
+
+	// Create input element for testing
+	var inputs = document.createElement('input');
+	
+	// Create the supports object
+	var supports = {};
+	
+	supports.autofocus   = 'autofocus' in inputs;
+	supports.required    = 'required' in inputs;
+	supports.placeholder = 'placeholder' in inputs;
+
+	// Fallback for autofocus attribute
+	if(!supports.autofocus) {
+		
+	}
+	
+	// Fallback for required attribute
+	if(!supports.required) {
+		
+	}
+
+	// Fallback for placeholder attribute
+	if(!supports.placeholder) {
+		
+	}
+	
+	// Change text inside send button on submit
+	var send = document.getElementById('submit-button');
+	if(send) {
+		send.onclick = function () {
+			this.innerHTML = '...Sending';
+		}
+	}
+
+})();
