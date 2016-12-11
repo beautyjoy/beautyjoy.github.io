dd3322f6b4c1e3450fc17bd249d43d1ed9b65f24
Zhicong <zhicong@berkeley.edu>
2016-12-10 16:45:17 -0800

    Implemented resource filtering.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
     normalizeGridHeights();
     fitHeaderText();
+
+    $('.resources-filter').change(function() {
+        var selectedTag = $(this).val();
+        if (selectedTag == "all") {
+            $(this).parents('.section').nextAll('.section').show();
+            $('.resources-list').find('li').show();
+        } else {
+            // Show only resources which have the selected filter as a class
+            $(this).parents('.section').nextAll('.section').hide();
+            $('.resources-list').find('li').each(function() {
+                if ($(this).hasClass(selectedTag)) {
+                    $(this).show();
+                    $(this).parents('.section').show();
+                }
+                else {
+                    $(this).hide();
+                }
+            })
+        }
+    });
 });
 
 $(window).on('resize orientation', function() {
