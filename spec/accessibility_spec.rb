ea4ed18ca694f0bf8deba824ce48c4b8f39f47ec
Michael Ball <michael@mball.co>
2024-06-17 14:08:08 -0700

    Accessibility specs correctly run on all pages with useful output

--- spec/accessibility_spec.rb
+++ spec/accessibility_spec.rb
@@ -1,43 +1,44 @@
 # Run accessibility specs for all pages in the webiste.
 # This runs the axe accessibility checker on each page in a headless browser.
-# This assumes that the website is running locally on port 4000.
 
-# TODO: See if we can share this with spec_helper.rb
+# spec_helper ensures the webiste is built and can be served locally
+
 require 'yaml'
 require 'spec_helper'
 
 # RSPEC_CONFIG_FILE = '_config.yml' or ENV['RSPEC_CONFIG_FILE']
 
 def site_url
-  YAML.load_file(RSPEC_CONFIG_FILE)['url']
+  @site_url ||= YAML.load_file(RSPEC_CONFIG_FILE)['url']
 end
 
 def load_site_urls
-  deploy_url = site_url
+  puts "Running accessibility tests, expected deploy URL: #{site_url}"
   # TODO: Handle case where build is not in _site
   sitemap_text = File.read('_site/sitemap.xml')
   sitemap_links = sitemap_text.scan(/<loc>.+<\/loc>/)
   sitemap_links.filter_map do |link|
-    link = link.gsub("<loc>#{deploy_url}", '')
-    link = link.gsub('</loc>', '')
+    link = link.gsub("<loc>#{site_url}", '').gsub('</loc>', '')
 
     next unless link.end_with?('.html') or link.end_with?('/')
 
     link
-  end
+  end.sort
 end
 
-$ALL_PAGES = load_site_urls
+ALL_PAGES = load_site_urls
+puts "Running tests on #{ALL_PAGES.count} pages."
+puts "\t- #{ALL_PAGES.join("\n\t- ")}\n#{'=' * 50}\n\n"
 
-$ALL_PAGES.each do |page|
-  describe "Page #{page} is accessible", type: :feature, js: true do
+ALL_PAGES.each do |path|
+  describe "Page '#{path}' is accessible", type: :feature, js: true do
     before(:each) do
-      visit(page)
+      visit(path)
     end
 
     it "according to WCAG 2.0 AA (REQUIRED)" do
       expect(page).to be_axe_clean.according_to(
-        :wcag2aa, "path: #{page} does NOT meet WCAG 2.0 AA standards"
+        :wcag2aa, "path: #{path} does NOT meet WCAG 2.0 AA standards"
       )
     end
   end
